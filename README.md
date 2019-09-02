099b849f88c3ee475c86153f947a223b8d0316d2
Brycen Dorsay <bdorsay@baileynelson.com>
2019-09-02 16:07:22 -0700

    install script

--- README.md
+++ README.md
@@ -6,16 +6,21 @@ Easily switch directories to the desired github repository and clone if necessar
 
 ### wget
 
-`wget -O - https://github.com/Bailey-Nelson/switch/releases/download/v0.1-alpha/install.sh | bash`
+`wget -O - https://github.com/Bailey-Nelson/switch/releases/download/v0.2-alpha/install.sh | bash`
 
 ### curl
 
-`curl https://github.com/Bailey-Nelson/switch/releases/download/v0.1-alpha/install.sh | bash`
+`curl -L -o install.sh https://github.com/Bailey-Nelson/switch/releases/download/v0.2-alpha/install.sh | bash`
 
 ### git
 
 1. git clone
 2. add alias to bash_aliases
-3. source .bash_profile or .bash_rc
+3. source .bash_profile or .bashrc
+
+## Assumptions
+
+If .bash_profile exists, use that. Otherwise use .bashrc
+A .bash_aliases file will be created and linked to if one does not exist
 
 NOTE: Make sure you trust this script and that it's not editing your hosts nefariously. I can tell you it's safe but you shouldn't trust strangers on the internet.
--- install.sh
+++ install.sh
@@ -1,36 +1,48 @@
 #!/usr/bin/env bash
 CONFIG_FILE=~/.bnswitch
+platform=`uname`
+profile='unknown'
+aliases=~/.bash_aliases
+
+# create the config file for `switch` if it doesn't exist
 [ ! -f "$CONFIG_FILE" ] && touch ~/.bnswitch
 
-# DIR=~/dev/github.com/bailey-nelson/switch
-DIR=~/dev/github.com/bailey-nelson/switch/test
+# create ~/.bash_aliases if it doesn't exist
+[ ! -d $aliases ] && touch $aliases
 
+# directory where `switch` will live
+DIR=~/dev/github.com/bailey-nelson/switch
+
+# create the dir if it does not exist
 [ ! -d $DIR ] && mkdir -p $DIR
+
+# download `switch`
 cd $DIR
-curl -L -o switch https://github.com/Bailey-Nelson/switch/releases/download/v0.1-alpha/switch
+curl -L -o switch https://github.com/Bailey-Nelson/switch/releases/download/v0.2-alpha/switch
+# TODO: check that the downloaded file matches the signature
 chmod +x switch
-# platform=`uname`
-# profile='unknown'
-# aliases=~/.bash_aliases
 
-# if [ -d ~/.bashrc ]; then
-#   profile='.bashrc'
-# elif [ -d ~/.bash_profile ]; then
-#   profile='.bash_profile'
-# fi
+# find which bash profile to update
+if [ -f ~/.bash_profile ]; then
+  profile='.bash_profile'
+elif [ -f ~/.bashrc ]; then
+  profile='.bashrc'
+fi
 
-# [ ! -d ~/.bash_aliases ] && touch ~/.bash_aliases
+# update ~/.bash_aliases with the required alias
+if ! grep -Fxq "alias switch='source $DIR/switch'" $aliases; then
+  echo '# bailey-nelson/switch - `source` is required in order to change directories in a shell script' >> $aliases
+  echo "alias switch='source $DIR/switch'" >> $aliases
+fi
 
-# if ! grep -Fxq "alias switch=source $DIR" ~/.bash_aliases; then
-#   echo -n '# bailey-nelson/switch - `source` is required in order to change directories in a shell script' >> ~/.bash_aliases
-#   echo "alias switch=source $DIR" >> ~/.bash_aliases
-# fi
+# add a reference to ~/.bash_aliases if one does not already exist
+if ! grep -q "[ -f $aliases ]" ~/$profile; then
+  echo $'# Aliases go in their own file
+if [ -f ~/.bash_aliases ]; then
+  . ~/.bash_aliases
+fi
+' >> ~/$profile
+fi
 
-# if ! grep -Fq "[ -f ~/.bash_aliases ]" $profile; then
-#   echo $'
-# # Aliases go in their own file
-# if [ -f ~/.bash_aliases ]; then
-#   . ~/.bash_aliases
-# fi
-# ' >> ~/$profile
-# fi
+echo "complete! you may need to source your ~/$profile before using switch"
+echo "  example: $ . ~/$profile"
